b99afcd9a976f88e87f0dfd809e9fc1a7b7d462e
Andrea Ambrose <andreamoesha@gmail.com>
2025-03-18 21:30:09 -0500

    height works

--- calculator.py
+++ calculator.py
@@ -2,47 +2,46 @@ def get_user_input():
     feet = input("What's the height in feet? ")
     inches = input("And inches? ")
     pounds = input("What's the weight in pounds? ")
-    return int(feet), int(inches), float(pounds)
+    return float(feet), float(inches), float(pounds)
 
-def height_to_meters(feet: int, inches: int):
-    return
-    # return ((feet*12) + inches) * 0.025 # metric covnersion factor for meters
+def height_to_meters(feet: float, inches: float):
+    return ((feet*12) + inches) * 0.025 # metric covnersion factor for meters
 
-def pounds_to_kg(pounds):
-    return
-    # return pounds *0.45 # metric conversion factor for kilograms
+# def pounds_to_kg(pounds):
+#     return
+#     return pounds *0.45 # metric conversion factor for kilograms
 
-def bmicalculator(height, weight: float):
-    height = height * height # square height in meters
-    bmi = weight / height #BMI Formula: kg div by squared height
-    # decimal precision
-    bmi_decimal = float("{:.2f}".format(bmi))
-    return #bmi_decimal 
+# def bmicalculator(height, weight: float):
+#     height = height * height # square height in meters
+#     bmi = weight / height #BMI Formula: kg div by squared height
+#     # decimal precision
+#     bmi_decimal = float("{:.2f}".format(bmi))
+#     return bmi_decimal 
 
-def categorycalculator(bmi):
-    # match bmi:
-    #     case _ if bmi < 18.5:
+# def categorycalculator(bmi):
+#     # match bmi:
+#     #     case _ if bmi < 18.5:
     
-    #     case _ if bmi < 18.5:
+#     #     case _ if bmi < 18.5:
         
-    #     case _ if bmi < 18.5:
+#     #     case _ if bmi < 18.5:
 
-    #     case _ if bmi < 18.5:
-    return
+#     #     case _ if bmi < 18.5:
+#     return
 
-def print_results(bmi, category):
-    print("BMI: " + bmi)
+# def print_results(bmi, category):
+#     print("BMI: " + bmi)
 
 
-# Gather data and calculate using that data
+# # Gather data and calculate using that data
 
-feet, inches, pounds = get_user_input()
-kg = pounds_to_kg(pounds)
-height = height_to_meters(feet, inches) # in meters
-bmi = bmicalculator(height, kg)
-category = categorycalculator(bmi)
+# feet, inches, pounds = get_user_input()
+# kg = pounds_to_kg(pounds)
+# height = height_to_meters(feet, inches) # in meters
+# bmi = bmicalculator(height, kg)
+# category = categorycalculator(bmi)
 
-# Print results
-print_results(bmi, category)
+# # Print results
+# print_results(bmi, category)
 
 
--- test_calculator.py
+++ test_calculator.py
@@ -1,27 +1,28 @@
 import pytest
-from calculator import get_user_input, height_to_meters, pounds_to_kg, bmicalculator, categorycalculator, print_results
+from calculator import get_user_input, height_to_meters#, pounds_to_kg, bmicalculator, categorycalculator, print_results
 
 def test_user_input(monkeypatch): # monekypatch needed for i/o
-    ft = 5
-    inches = 3
+    ft = 5.0
+    inches = 3.0
     lbs = 125.0
 
     input_values = iter([ft, inches, lbs])
     monkeypatch.setattr('builtins.input', lambda _: next(input_values))
     result = get_user_input()
-    assert result == (5, 3, 125.0)
+    assert result == (5.0, 3.0, 125.0)
 
 def test_height_to_meters():
-    assert height_to_meters() == "test"
+    result = height_to_meters(5.0,3.0)
+    assert result == 1.5750000000000002
 
-def test_pounds_to_kg():
-    assert pounds_to_kg() == "test"
+# def test_pounds_to_kg():
+#     assert pounds_to_kg() == "test"
 
-def test_bmicalculator():
-    assert bmicalculator() == "test"
+# def test_bmicalculator():
+#     assert bmicalculator() == "test"
 
-def test_categorycalculator():
-    assert categorycalculator == "tes"
+# def test_categorycalculator():
+#     assert categorycalculator == "tes"
 
-def test_print_results():
-    assert print_results == "test"
+# def test_print_results():
+#     assert print_results == "test"
